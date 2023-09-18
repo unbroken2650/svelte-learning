069383915770905748cebe3bb204f79994dad494
UnbroKen <unbroken2650@gmail.com>
2023-09-18 01:31:56 -0400

    delete unnecessary thing

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+  let list = [
+		{id:0, content:'Task #0', done: false},
+		{id:1, content:'Task #1', done: false},
+		{id:2, content:'Task #2', done: true},
+		{id:3, content:'Task #3', done: false}
+	]
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<ul>
+	{#each list as item}
+	<li>
+		<p>{item.id}</p>
+		<p>{item.content}</p>
+		<p>{item.done}</p>
+	</li>
+	{/each}
+</ul>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
